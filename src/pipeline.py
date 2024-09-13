091953f96343d0108673a33ad9e2335169a2a7c6
Toan-Do <dvtoan7997@gmail.com>
2024-09-13 21:49:18 +0700

    fix: pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -15,7 +15,7 @@ def load_pipeline() -> StableDiffusionXLPipelineFaster:
     ).to("cuda")
     pipeline = compile_pipe(pipeline)
     load_pipe(pipeline, dir="models/newdream-sdxl-20-optimized")
-    pipeline(prompt="")
+    pipeline(prompt="", cache_interval=2, cache_layer_id=0, cache_block_id=0)
 
     return pipeline
 
